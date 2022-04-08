0a1d93e11ba42d4dba27e095c1f05fc176aefa12
Aashiq Ahmed <ahmed.aashiq23@gmail.com>
2022-04-08 11:58:23 +0530

    New Fields added to Tables

--- database/migrations/2022_04_08_053411_create_vehicle_addons_table.php
+++ database/migrations/2022_04_08_053411_create_vehicle_addons_table.php
@@ -15,7 +15,28 @@ return new class extends Migration
     {
         Schema::create('vehicle_addons', function (Blueprint $table) {
             $table->id();
+
+            // @todo - Create a many to many relationship between vehicle and addons
+            
+            // $table->bigInteger('product_id')->nullable();
+            // $table->foreign('product_id')->references('id')->on('products');
+
+            $table->bigInteger('manufacturer_id')->nullable();
+            $table->foreign('manufacturer_id')->references('id')->on('manufacturers');
+
+            $table->bigInteger('vehicle_model_id')->nullable();
+            $table->foreign('vehicle_model_id')->references('id')->on('vehicle_models');
+
+            $table->string('name'); //varchar 255
+
+            $table->longText('description');// text
+
+            $table->string('image')->nullable();// varchar 255
+
+            $table->boolean('is_active')->default(true);// tinyint 2 - 0 or 1
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2022_04_08_054013_create_spareparts_table.php
+++ database/migrations/2022_04_08_054013_create_spareparts_table.php
@@ -15,7 +15,26 @@ return new class extends Migration
     {
         Schema::create('spareparts', function (Blueprint $table) {
             $table->id();
+            
+            $table->bigInteger('product_id')->nullable();
+            $table->foreign('product_id')->references('id')->on('products');
+
+            $table->bigInteger('manufacturer_id')->nullable();
+            $table->foreign('manufacturer_id')->references('id')->on('manufacturers');
+
+            $table->bigInteger('vehicle_model_id')->nullable();
+            $table->foreign('vehicle_model_id')->references('id')->on('vehicle_models');
+
+            $table->string('name'); //varchar 255
+
+            $table->longText('description');// text
+
+            $table->string('image')->nullable();// varchar 255
+
+            $table->boolean('is_active')->default(true);// tinyint 2 - 0 or 1
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
